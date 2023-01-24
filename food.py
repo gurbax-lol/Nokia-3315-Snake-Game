61cec60918c2e211526b576a521af8ebe320687f
Gurbax <hi@gurbax.lol>
2023-01-24 19:54:09 +0530

    Detect when snake collides with itself

--- food.py
+++ food.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
-from random import randint
+from random import randint, choice
+from time import sleep
 #TODO: Generate Super Food
+
+# SUPER_FOOD_COLORS = ["medium violet red", "orange", "navajo white", "SteelBlue1"]
 
 
 class Food(Turtle):
@@ -10,10 +13,26 @@ class Food(Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("white")
-        self.speed("fastest")
         self.generate_food()
 
     def generate_food(self):
         rand_x = randint(-280, 280)
         rand_y = randint(-280, 280)
         self.goto(rand_x, rand_y)
+
+
+# class SuperFood(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.shape("triangle")
+#         self.penup()
+#         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+#         self.color(choice(SUPER_FOOD_COLORS))
+#         self.generate_food()
+#
+#
+#     def generate_food(self):
+#         rand_x = randint(-280, 280)
+#         rand_y = randint(-280, 280)
+#         self.goto(rand_x, rand_y)
+#
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from turtle import Screen
 from time import sleep
 from snake import Snake
 from food import Food
+# from food import SuperFood
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
-# screen.bgcolor("black")
 screen.tracer(0)
 screen.bgcolor("dark olive green")
 screen.title("Nokia 3315 Snake Game")
@@ -31,12 +31,15 @@ def detect_wall_collision():
 
 
 def loop_over_walls():
-    if snake.head.xcor() > 290:  # If the snake has reached the left edge,
-        snake.head.goto(-290, snake.head.ycor())  # loop over to the right edge.
-    if snake.head.xcor() < -290:  # If the snake has reached the right edge,
-        snake.head.goto(290, snake.head.ycor())  # loop over to the left edge.
+    if snake.head.xcor() > 290:  # If the snake has reached the right edge,
+        snake.head.goto(-290, snake.head.ycor())  # loop over to the left edge.
+
+    if snake.head.xcor() < -290:  # If the snake has reached the left edge,
+        snake.head.goto(290, snake.head.ycor())  # loop over to the right edge.
+
     if snake.head.ycor() > 290:  # If the snake has reached the up edge,
         snake.head.goto(snake.head.xcor(), -290)  # loop over to the down edge.
+
     if snake.head.ycor() < -290:  # If the snake has reached the down edge,
         snake.head.goto(snake.head.xcor(), 290)  # loop over to the up edge.
 
@@ -49,10 +52,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.generate_food()
         scoreboard.increase_score()
+        snake.extend()
     # Detect running into a wall,
     # detect_wall_collision()
     # Start from the opposite edge if the snake has reached the screen's edge
-    # loop_over_walls()
+    loop_over_walls()
+    # Detect collision with self
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 
